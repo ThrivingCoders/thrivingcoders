48a591a30d98dd3790f769a40e94876829132bce
Chris <chrismorgan@airstriptech.com>
2013-11-24 08:46:45 -0600

    Initial commit of find my ip. Work in progress.

---
+++ ThrivingCodersl.GetMyIp/ThrivingCodersl.GetMyIp/IpResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace ThrivingCodersl.GetMyIp
+{
+    public class IpResponse
+    {
+        public string ip { get; set; } 
+    }
+}
---
+++ ThrivingCodersl.GetMyIp/ThrivingCodersl.GetMyIp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+
+namespace ThrivingCodersl.GetMyIp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+//            httpClient.BaseAddress = new Uri("http://ip.jsontest.com");
+
+            var httpResponse = httpClient.GetAsync("http://ip.jsontest.com");
+            var task = httpResponse.Result.Content.ReadAsAsync<IpResponse>();
+            
+            var ipResponse = task.Result;
+            Console.WriteLine(ipResponse.ip);
+            Console.ReadKey();
+
+        }
+    }
+}
